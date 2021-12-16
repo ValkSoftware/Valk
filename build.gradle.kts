9f74ff310891853de8b3c3672eed68e62b45c8a0
mrsherobrine <mrsherobrinenaomi@gmail.com>
2021-12-17 00:24:52 +0100

    don't you just love it when it throws errors with no solutions

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,9 @@ application {
 }
 
 tasks.withType(CppCompile::class.java).configureEach {
+    compilerArgs.add("-pthread")
     compilerArgs.add("-DSPDLOG_HEADER_ONLY") // spdlog dependency
     compilerArgs.add("-DASIO_STANDALONE") // asio dependency
     compilerArgs.add("-std=c++17")
-    compilerArgs.add("-pthread")
+    compilerArgs.add("-levpp")
 }
--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
-#include <asio.hpp>
+#include <spdlog/spdlog.h>
+#include <pthread.h>
+#include <evpp/tcp_server.h>
+#include <evpp/buffer.h>
+#include <evpp/tcp_conn.h>
 
 #include "server.h"
 #include "arguments.h"
@@ -12,18 +16,31 @@ int main(int argc, char *argv[])
     Server* s = new Server();
     s->startup_sequence();
 
-    using asio::ip::tcp;
+    // TODO replace port with config
+    std::string addr = "0.0.0.0:25565";
 
-    asio::io_context net_thread_context;
-    tcp::acceptor conn_acceptor(net_thread_context, tcp::endpoint(tcp::v4(), 25565)); // TODO replace port with config
+    int net_threads = 4;
+    evpp::EventLoop loop;
+    evpp::TCPServer server(&loop, addr, "valk", net_threads);
+    
+    server.SetMessageCallback([](const evpp::TCPConnPtr& conn, evpp::Buffer* msg)
+    {   
+        spdlog::info("received {}, sending it back", msg->ToString());
+        conn->Send(msg);
+    });
 
-    for(;;)
+    server.SetConnectionCallback([](const evpp::TCPConnPtr& conn) 
     {
-        tcp::socket socket(net_thread_context);
-        conn_acceptor.accept(socket);
-        asio::error_code ignore;
-        asio::write(socket, asio::buffer("YO"), ignore);
-    }
+        if (conn->IsConnected()) {
+            std::cout << "new connection from " << conn->remote_addr();
+        } else {
+            std::cout << "connection from " << conn->remote_addr() << "closed";
+        }
+    });
+
+    server.Init();
+    server.Start();
+    loop.Run();
 
     return 0;
 }
