257555e1c3772f9affdd712a7e04b6e61742bf55
Felenov <68743549+Felenov@users.noreply.github.com>
2021-12-31 09:15:37 +0300

    Fix ASCII art to match logo
    remove 2 space per line.

--- server/server.cpp
+++ server/server.cpp
@@ -10,13 +10,13 @@ Server::~Server() {}
 void Server::startup_sequence()
 {
     spdlog::info("-------------------------------------------");
-    spdlog::info("   █   █");
-    spdlog::info("  ██   ██    Valk Software's valk.");
-    spdlog::info("  █     █    Eagle-eyed perfection.");
-    spdlog::info(" ██  █  ██");
-    spdlog::info("  █     █    © 2021-2022");
-    spdlog::info("  ██   ██    Version: 0.0.1 (pre-alpha)");
-    spdlog::info("   █   █");
+    spdlog::info("   █ █");
+    spdlog::info("  ██ ██    Valk Software's valk.");
+    spdlog::info("  █   █    Eagle-eyed perfection.");
+    spdlog::info(" ██ █ ██");
+    spdlog::info("  █   █    © 2021-2022");
+    spdlog::info("  ██ ██    Version: 0.0.1 (pre-alpha)");
+    spdlog::info("   █ █");
     spdlog::info("-------------------------------------------");
     spdlog::info("starting up...");
 
